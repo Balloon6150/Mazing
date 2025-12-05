98762fffa1c5141969b7cdb44f724185642c4a48
Balloon6150 <tristan.loland@gmail.com>
2025-12-04 20:38:05 -0800

    modified:   Assets/MovementScript.cs

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+    public int xspeed = 0;
+    public int yspeed = 0;
+    public int movementSpeed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        // Get speed to decrease every second rather than every frame
+        if (xspeed > 0) {
+            xspeed -= 1;
+        }
+        if (yspeed > 0) {
+            yspeed -= 1;
+        }
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.Translate(movement * Time.deltaTime);
+        // Controls with WASD
+        if (Input.GetKey(KeyCode.W))
+        {
+            yspeed += movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            yspeed -= movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            xspeed -= movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            xspeed += movementSpeed;
+        }
+        
+        // Object movement
+
+        transform.Translate(new Vector3(xspeed * Time.deltaTime, yspeed * Time.deltaTime, 0));
     }
 }
